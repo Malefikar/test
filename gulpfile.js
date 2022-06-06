94a5f91c6e0caf1829321b18c7922e24f1b4d5b4
Malefikar <pavel721@mail.ru>
2022-06-06 13:45:10 +0700

    2

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import rename from 'gulp-rename';
+import svgo from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
+import del from 'del';
 
 // Styles
 
@@ -17,6 +21,57 @@ export const styles = () => {
     .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
+
+// HTML
+
+const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(gulp.dest('build'));
+}
+
+  // Images
+
+const copyImages = () => {
+  return gulp.src('source/img/**/*.{png,jpg}')
+  .pipe(gulp.dest('build/img'))
+}
+
+  // SVG
+
+  const svg = () =>
+  gulp.src(['source/img/*.svg', '!source/img/icons/*.svg'])
+    .pipe(svgo())
+    .pipe(gulp.dest('build/img'));
+
+const sprite = () => {
+  return gulp.src('source/img/*.svg')
+    .pipe(svgo())
+    .pipe(svgstore({
+      inlineSvg: true
+  }))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('build/img'));
+  }
+
+    // Copy
+
+const copy = (done) => {
+  gulp.src([
+    'source/fonts/*.{woff2,woff}',
+    'source/*.ico',
+    'source/*.webmanifest'
+  ], {
+  base: 'source'
+  })
+    .pipe(gulp.dest('build'))
+    done();
+  }
+
+  // Clean
+
+const clean = () => {
+  return del('build');
+  };
 
 // Server
 
@@ -32,14 +87,32 @@ const server = (done) => {
   done();
 }
 
+  // Reload
+
+  const reload = (done) => {
+    browser.reload();
+    done();
+    }
+
 // Watcher
 
 const watcher = () => {
   gulp.watch('source/less/**/*.less', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch('source/*.html', gulp.series(html, reload));
 }
 
 
 export default gulp.series(
-  styles, server, watcher
-);
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    styles,
+    html,
+    svg,
+    sprite,
+  ),
+  gulp.series(
+    server,
+    watcher
+  ));
